1ae2192296a6a45373f4a3968fd5a782d3b8ae66
Lucas Walter <wsacul@gmail.com>
2023-12-28 09:04:24 -0800

    more concise log output test fixes

--- test/test_rospy/test/rostest/test_rospy_client_online.py
+++ test/test_rospy/test/rostest/test_rospy_client_online.py
@@ -101,17 +101,17 @@ class TestRospyClientOnline(unittest.TestCase):
             
             rospy.logwarn("test 2")
             lerr_last = lerr.getvalue().splitlines()[-1]
-            self.assert_("[WARN]" in lerr_last)
+            self.assert_("[W]" in lerr_last)
             self.assert_("test 2" in lerr_last)
 
             rospy.logerr("test 3")
             lerr_last = lerr.getvalue().splitlines()[-1]
-            self.assert_("[ERROR]" in lerr_last)
+            self.assert_("[E]" in lerr_last)
             self.assert_("test 3" in lerr_last)
 
             rospy.logfatal("test 4")
             lerr_last = lerr.getvalue().splitlines()[-1]
-            self.assert_("[FATAL]" in lerr_last)
+            self.assert_("[F]" in lerr_last)
             self.assert_("test 4" in lerr_last)
 
             # logXXX_once
@@ -307,17 +307,17 @@ class TestRospyClientOnline(unittest.TestCase):
             
             rospy.logwarn("test child logger 2", logger_name="log2")
             lerr_last = lerr.getvalue().splitlines()[-1]
-            self.assert_("[WARN]" in lerr_last)
+            self.assert_("[W]" in lerr_last)
             self.assert_("test child logger 2" in lerr_last)
 
             rospy.logerr("test child logger 3", logger_name="log3")
             lerr_last = lerr.getvalue().splitlines()[-1]
-            self.assert_("[ERROR]" in lerr_last)
+            self.assert_("[E]" in lerr_last)
             self.assert_("test child logger 3" in lerr_last)
 
             rospy.logfatal("test child logger 4", logger_name="log4")
             lerr_last = lerr.getvalue().splitlines()[-1]
-            self.assert_("[FATAL]" in lerr_last)
+            self.assert_("[F]" in lerr_last)
             self.assert_("test child logger 4" in lerr_last)
 
         finally:
